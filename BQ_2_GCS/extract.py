4b1a18281b110e3d1fd50e9149688a8a34885bb2
gmflau <gmflau@gmail.com>
2023-07-01 21:54:41 -0700

    First Drop of extract

---
+++ BQ_2_GCS/extract.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from google.cloud import bigquery
+client = bigquery.Client()
+bucket_name = 'glau-test-bucket-us-central1'
+
+now = datetime.now()
+project="bigquery-public-data"
+dataset_id="austin_311"
+dataset_id="austin_311"
+table_name="311_service_requests"
+destination_uri = "gs://{}/{}".format(bucket_name, "austin_crimes_" + now.strftime("%Y%m%d") + ".json")
+dataset_ref = bigquery.DatasetReference(project, dataset_id)
+table_ref = dataset_ref.table(table_name)
+job_config = bigquery.job.ExtractJobConfig()
+job_config.destination_format = bigquery.DestinationFormat.NEWLINE_DELIMITED_JSON
+
+extract_job = client.extract_table(
+    table_ref,
+    destination_uri,
+    job_config=job_config,
+    # Location must match that of the source table.
+    location="US",
+)  # API request
+extract_job.result()  # Waits for job to complete.
